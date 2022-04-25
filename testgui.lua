acecaaab5eca37afcb9fd628f821044ee32588f9
k-rgz <98106637+k-rgz@users.noreply.github.com>
2022-04-25 16:29:20 +0200

    Create testgui.lua

---
+++ testgui.lua
@@ -0,0 +1,23 @@
+-- epic#2222
+
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+local UICorner = Instance.new("UICorner")
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+TextLabel.Parent = ScreenGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 0.150
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.421911418, 0, 0.0937996805, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "Test GUI made by epic#2222"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+UICorner.Parent = TextLabel
